28f13046bf1638695487a9e90da6716f62cb4cf5
Iris <linlaephway@gmail.com>
2023-08-13 15:24:28 +0800

    Word occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,16 +1,13 @@
-def main():
-    user_input = input("Text: ")
-    words = user_input.split()
-    word_counts = {}
+text = input("Text: ")
+words = text.split()
+word_counts = {}
 
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
+for word in words:
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
 
-    for word, count in sorted(word_counts.items()):
-        print(f"{word} : {count}")
+for word, count in sorted(word_counts.items()):
+    print(f"{word} : {count}")
 
-
-main()
